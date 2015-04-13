282f6b112cee26435e56b139234537dcc5a74806
Marcus S. <m4rkuz@gmail.com>
2015-04-13 16:50:31 -0500

    Update GeneratorLimitAdvisor.java
    Add comments, to clarify why to keep this un-used class

--- poem-generator/src/main/java/co/m4solutions/grammars/generator/GeneratorLimitAdvisor.java
+++ poem-generator/src/main/java/co/m4solutions/grammars/generator/GeneratorLimitAdvisor.java
@@ -1,5 +1,6 @@
 package co.m4solutions.grammars.generator;
-
+//This class is not currently used by the poem generator, 
+//it will be added when grammars of over 256 rules are accepted by the generator.
 public class GeneratorLimitAdvisor {
 	
 	public int numberOfNonTerminalTokens;
