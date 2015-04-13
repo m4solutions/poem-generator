632d950a786c09fb6eb65f8e9f6072c28816a042
Marcus S. <m4rkuz@gmail.com>
2015-04-13 16:54:19 -0500

    Update RandomTokenGroupChooser.java
    Add comments to clarify the importance of the class

--- poem-generator/src/main/java/co/m4solutions/grammars/generator/RandomTokenGroupChooser.java
+++ poem-generator/src/main/java/co/m4solutions/grammars/generator/RandomTokenGroupChooser.java
@@ -8,21 +8,11 @@ public class RandomTokenGroupChooser {
 
 	public static Token choose(TokenGroup group, GeneratorLimitAdvisor advisor) {
 		if (group.getOperator().equals(TokenOperator.OR)){//The only one by now
-		    
-		    
-		    
+		
+			//Here is where the "magic" happens, for the next version this choice should be driven 
+			//not by random numbers, but by hints of gramatically correctnes secuences.
 			int choosenTokenIndex = 0 + (int)(Math.random() * ((group.getTokens().size()-1) + 1));
 			Token choosen = group.getTokens().get(choosenTokenIndex);
-			
-//			if(choosen instanceof NonTerminalToken){
-//			     choosenTokenIndex = 0 + (int)(Math.random() * ((group.getTokens().size()-1) + 1));
-//	             choosen = group.getTokens().get(choosenTokenIndex);
-//			}
-//			
-//	        if(choosen instanceof NonTerminalToken){
-//                 choosenTokenIndex = 0 + (int)(Math.random() * ((group.getTokens().size()-1) + 1));
-//                 choosen = group.getTokens().get(choosenTokenIndex);
-//            }
 			return choosen;
 		}
 		return group;
